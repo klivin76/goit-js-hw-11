881ea23a0d35377e89562cb25ad943409fa175d1
Klementiev Ivan <klivin76@gmail.com>
2023-03-12 11:45:57 +0200

    create import

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import {createImagesMarkup} from './js/create_images';
+import { FetchAPI } from './js/fetchAPI';
+import {simplelightbox} from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import {UrlGallery} from './js/urlgallery';
